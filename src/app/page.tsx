78ca6427197b5a6f58532a651f2f65c9194d28a5
PierreT <pierre.tusseau@gmail.com>
2025-01-04 18:56:48 +0100

    Revamp a little home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,36 +7,51 @@ export default function Home() {
 
   return (
     <div className="bg-neutral-950 text-neutral-50 h-screen flex flex-col gap-10">
-      <main className="flex flex-wrap gap-8 p-4">
-        <HomeLink
-          href="/amaw-sidebar"
-          title="AMAW Sidebar"
-          subtitle="432x1440"
-        />
-        <HomeLink
-          href="/amaw-tracker"
-          title="AMAW Tracker"
-          subtitle="2560x200"
-        />
-        <HomeLink
-          href="/amaw-table"
-          title="AMAW Table"
-        />
-        <HomeLink
-          href="/amaw-monsters"
-          title="AMAW Monsters"
-          subtitle="1250x980"
-        />
-        <HomeLink
-          href="/amaw-weapons"
-          title="AMAW Weapons"
-          subtitle="220x1104"
-        />
-        <HomeLink
-          href="/amaw-last-hunt"
-          title="AMAW Last Hunt"
-          // subtitle="220x1104"
-        />
+      <main className="flex flex-col gap-12 p-4">
+        <section>
+          <h2>Decorations</h2>
+          <div className="flex flex-wrap gap-8">
+            <HomeLink
+              href="/announces"
+              title="Announces"
+              subtitle="400x100"
+            />
+          </div>
+        </section>
+        <section>
+          <h2>MHWI - All Weapons All Monsters</h2>
+          <div className="flex flex-wrap gap-8">
+            <HomeLink
+              href="/amaw-sidebar"
+              title="AMAW Sidebar"
+              subtitle="432x1440"
+            />
+            <HomeLink
+              href="/amaw-tracker"
+              title="AMAW Tracker"
+              subtitle="2560x200"
+            />
+            <HomeLink
+              href="/amaw-table"
+              title="AMAW Table"
+            />
+            <HomeLink
+              href="/amaw-monsters"
+              title="AMAW Monsters"
+              subtitle="1250x980"
+            />
+            <HomeLink
+              href="/amaw-weapons"
+              title="AMAW Weapons"
+              subtitle="220x1104"
+            />
+            <HomeLink
+              href="/amaw-last-hunt"
+              title="AMAW Last Hunt"
+              // subtitle="220x1104"
+            />
+          </div>
+        </section>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center mt-auto">
         &copy;{date.getFullYear()} Pierre &quot;Kobaru&quot; Tusseau
