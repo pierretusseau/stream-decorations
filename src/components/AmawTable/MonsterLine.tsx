c0e45008565e15abba272b58ac08495a23a9640d
PierreT <pierre.tusseau@gmail.com>
2025-01-17 10:55:53 +0100

    AMAW Table fix monster unlock

--- src/components/AmawTable/MonsterLine.tsx
+++ src/components/AmawTable/MonsterLine.tsx
@@ -13,11 +13,12 @@ function MonsterLine({
   weapons: Weapon[]
 }) {
   const hunts = useHuntStore((state) => state.hunts)
-  const [monsterUnlocked, setMonsterUnlocked] = useState<boolean>(monster.unlocked)
+  const [monsterUnlocked, setMonsterUnlocked] = useState<boolean>(false)
   const monsterHunts = hunts.filter(hunt => hunt.monster === monster.id)
   const service_key = useSettingsStore((state) => state.supabase_service_key)
 
   const handleMonsterLockState = useCallback(async (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    console.log(service_key)
     if (service_key.length === 0) return
     e.preventDefault()
     await fetch(`${window.location.origin}/api/amaw/unlock-monster`, {
@@ -42,19 +43,27 @@ function MonsterLine({
       })
   }, [service_key, monster.id, monster.name, setMonsterUnlocked])
 
+  if (!monsterUnlocked && monster.unlocked) setMonsterUnlocked(true)
+
   return (
-    <div
-      className="group/monster-line bg-neutral-950 flex w-full py-2 pl-2 hover:bg-neutral-900 transition"
-    >
+    <div className={`group/monster-line ${[
+      'bg-neutral-950 flex w-full py-2 pl-2 hover:bg-neutral-900 transition',
+      !monsterUnlocked && 'opacity-50',
+    ].join(' ')}`}>
       <div
         className="group/monster-line-img w-[50px] mr-2 flex items-center"
         onContextMenu={(e) => handleMonsterLockState(e)}
       >
-        <div className="relative">
+        <div className="relative pointer-events-none">
           <MonsterImage
             monster={monster}
           />
-          {!monsterUnlocked &&<LockClosedIcon className="size-8 absolute top-1/2 left-1/2 -translate-y-[50%] -translate-x-[50%] opacity-75 pointer-events-none" />}
+          {!monsterUnlocked &&<LockClosedIcon
+            className={`${[
+              "size-8 absolute top-1/2 left-1/2 -translate-y-[50%] -translate-x-[50%]",
+              "opacity-75 z-10"
+            ].join(' ')}`}
+          />}
         </div>
       </div>
       {weapons.map((weapon, index: number) => {
